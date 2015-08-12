4567217fd7551805b067a5e382ee4ed5030756d3
Stanislas MICHALAK <smichalak@aldebaran.com>
2015-08-12 11:40:13 +0200

    Add xar python injector + first build.

--- src/com/aldebaran/intellij/lang/file/xar/AldebaranXarLanguage.java
+++ src/com/aldebaran/intellij/lang/file/xar/AldebaranXarLanguage.java
@@ -1,12 +1,13 @@
 package com.aldebaran.intellij.lang.file.xar;
 
 import com.intellij.lang.Language;
+import com.intellij.lang.xml.XMLLanguage;
 
 public class AldebaranXarLanguage extends Language {
 
     public static final AldebaranXarLanguage INSTANCE = new AldebaranXarLanguage();
 
     protected AldebaranXarLanguage() {
-        super("Aldebaran behavior", "application/xml");
+        super(XMLLanguage.INSTANCE, "Aldebaran behavior", "application/xml");
     }
 }
--- src/com/aldebaran/intellij/lang/injector/XarPythonInjector.java
+++ src/com/aldebaran/intellij/lang/injector/XarPythonInjector.java
@@ -1,15 +1,24 @@
 package com.aldebaran.intellij.lang.injector;
 
+import com.intellij.lang.Language;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.InjectedLanguagePlaces;
 import com.intellij.psi.LanguageInjector;
 import com.intellij.psi.PsiLanguageInjectionHost;
+import com.intellij.psi.xml.XmlText;
 import org.jetbrains.annotations.NotNull;
 
 public class XarPythonInjector implements LanguageInjector {
 
     @Override
     public void getLanguagesToInject(@NotNull PsiLanguageInjectionHost host, @NotNull InjectedLanguagePlaces injectedLanguagePlaces) {
-        System.out.println(host);
-        System.out.println(injectedLanguagePlaces);
+        if (!"xar".equals(host.getContainingFile().getVirtualFile().getExtension())) return;
+        if (!(host instanceof XmlText)) return;
+
+        final XmlText xmlText = (XmlText) host;
+
+        if (xmlText.getContext().getNode().findLeafElementAt(1) != null && "content".equals(xmlText.getContext().getNode().findLeafElementAt(1).getText())) {
+            injectedLanguagePlaces.addPlace(Language.findLanguageByID("Python"), TextRange.from(0, host.getTextLength()), "<!CDATA[", "]]>");
+        }
     }
 }
